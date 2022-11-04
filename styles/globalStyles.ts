94e9a737b764e47dc783d2848d0cfd5baa8df743
Vic Flores <vicsito2014@gmail.com>
2022-11-04 14:36:30 -0600

    feat(styles): global styles and normalize was provided

---
+++ styles/globalStyles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  text-align: center;
+`;
+
+export const VideoContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const CanvasContainer = styled.canvas`
+  position: absolute;
+`;
